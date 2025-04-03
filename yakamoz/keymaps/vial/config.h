f8b9fa80e242a00f85d16a0547401cff57bca284
Marc Tran Du Trieu <38071131+marctdt@users.noreply.github.com>
2025-04-03 11:14:06 +0800

    Update config.h

--- yakamoz/keymaps/vial/config.h
+++ yakamoz/keymaps/vial/config.h
@@ -6,21 +6,21 @@
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 } 
 
-//#define WS2812_DI_PIN GP1
+#define WS2812_DI_PIN GP1
 
 
-#ifdef OLED_ENABLE
+//#ifdef OLED_ENABLE
 
 /* OLED DRIVER CONFIG */
-#define OLED_DISPLAY_64X128
-#define OLED_IC OLED_IC_SH1107
+//#define OLED_DISPLAY_64X128
+//#define OLED_IC OLED_IC_SH1107
 /* Lowering the pre charge period fixes coil whining for the used display modules. */
-#define OLED_PRE_CHARGE_PERIOD 0x22
-#define OLED_VCOM_DETECT 0x35
-#define OLED_TIMEOUT 15000
+//#define OLED_PRE_CHARGE_PERIOD 0x22
+//#define OLED_VCOM_DETECT 0x35
+//#define OLED_TIMEOUT 15000
 
-#endif // OLED_ENABLE
+//#endif // OLED_ENABLE
 
 /* Indicator LEDs */
-//#define LED_CAPS_LOCK_PIN GP25
-//#define LED_PIN_ON_STATE 1
+#define LED_CAPS_LOCK_PIN GP25
+#define LED_PIN_ON_STATE 1
